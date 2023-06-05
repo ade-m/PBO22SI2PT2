a02931d1a057e83db32cb9af517f69b949fb87c2
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-05 18:57:49 +0700

    cindy

--- PBO22SI2PT2/src/App.java
+++ PBO22SI2PT2/src/App.java
@@ -12,7 +12,7 @@ public class App {
         System.out.println(cindy);
     }
     public static void init(){
-        Mahasiswa cindy = new Mahasiswa("Cindy C.", 
+        Mahasiswa cindy = new Mahasiswa("Cindy C...", 
         "030812121",
         "Jl. xxx no xx kota Medan",
         "Sistem Informasi");
