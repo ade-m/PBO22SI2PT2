0e68c99d2490a3f1ae208dc18d08b9f861ada9ba
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-05 18:53:42 +0700

    konstruktor dan init

--- PBO22SI2PT2/src/App.java
+++ PBO22SI2PT2/src/App.java
@@ -1,5 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        init();
+        Mahasiswa owen = new Mahasiswa();
+        System.out.println(owen);
+        owen = new Mahasiswa("Owen Felix", "Jl xx Medan");
+        System.out.println(owen);
+        Mahasiswa cindy = new Mahasiswa("Cindy C.", 
+                    "030812121",
+                    "Jl. xxx no xx kota Medan",
+                    "Sistem Informasi");
+        System.out.println(cindy);
+    }
+    public static void init(){
+        Mahasiswa cindy = new Mahasiswa("Cindy C.", 
+        "030812121",
+        "Jl. xxx no xx kota Medan",
+        "Sistem Informasi");
+        Mahasiswa owen = new Mahasiswa("Owen F.", 
+        "030812121",
+        "Jl. xxx no xx kota Medan",
+        "Sistem Informasi");
+        Mahasiswa nicholas = new Mahasiswa("Nicholas", 
+        "030812121",
+        "Jl. xxx no xx kota Medan",
+        "Sistem Informasi");
+        Mahasiswa alfredo = new Mahasiswa("Alfredo", 
+        "030812121",
+        "Jl. xxx no xx kota Medan",
+        "Sistem Informasi");
+
     }
 }
