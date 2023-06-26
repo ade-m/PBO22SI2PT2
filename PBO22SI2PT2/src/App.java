c47998739bef7d96d7370f7a17972b66eb4f3c3c
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-26 20:02:45 +0700

    insert dan delete

--- PBO22SI2PT2/src/App.java
+++ PBO22SI2PT2/src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     public static ArrayList<Mahasiswa> mahasiswa = 
@@ -6,7 +7,43 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         init();
+       //cetakDataMahasiswa();
+        int pilihan =2;
+        switch(pilihan){
+            case 1:// input data
+                inputDataMhs();
+                break;
+            case 2:// delete data
+                hapusData(0);
+                break;
+            default:
+                System.out.println();
+        }
         cetakDataMahasiswa();
+
+    }
+    public static void hapusData(int i){
+        mahasiswa.remove(i);
+    }
+    public static void inputDataMhs(){
+        Scanner input = new Scanner(System.in);
+        String nama="",studentID="", prodi="",alamat="";
+        System.out.print("Nama\t:");
+        if(input.hasNextLine())
+            nama = input.nextLine();
+        System.out.print("StudentID\t:");
+        if(input.hasNextLine())
+            studentID = input.nextLine();
+        System.out.print("Prodi\t:");
+        if(input.hasNextLine())
+            prodi = input.nextLine();
+        System.out.print("Alamat\t:");
+        if(input.hasNextLine())
+            alamat = input.nextLine();
+       
+        mahasiswa.add(new Mahasiswa(nama, studentID, alamat, prodi));
+        input.close();
+        System.out.println();
     }
     public static void cetakDataMahasiswa(){
         for (Mahasiswa mahasiswa2 : mahasiswa) {
@@ -37,6 +74,9 @@ public class App {
         "030812121",
         "Jl. xxx no xx kota Medan",
         "Sistem Informasi"));
+
+        mahasiswa.remove(alfredo);
+        mahasiswa.remove(1);
 
 
     }
